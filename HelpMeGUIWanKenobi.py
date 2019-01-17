4f3ddc41f2bb34ec09015acb000c87a95de708a0
Luke Goodman <45367938+SolomonsFootsteps@users.noreply.github.com>
2019-01-17 10:57:17 -0500

    Update HelpMeGUIWanKenobi.py
    ...yeah, turns out that decorator doesn't actually do anything

--- HelpMeGUIWanKenobi.py
+++ HelpMeGUIWanKenobi.py
@@ -4,6 +4,7 @@ import tkinter as tk
 
 # I definitely AM terrible at decorators and I'd like to not be
 
+# @grid() is almost working, but not quite
 def grid(num_rows, num_columns):
     def real_decorator(func):
         def wrapper(self, *args, **kwargs):
@@ -21,18 +22,53 @@ class TkRoot(tk.Tk):
         tk.Tk.__init__(self)
         main_window = MainWindow(parent=self)
         main_window.grid()
+        SecondWindow(parent=self)
+        ThirdWindow(parent=self)
+        
+
+# this test case makes it seem like @grid(10,10) works, but it really doesn't
 
 class MainWindow(tk.Frame):
     @grid(10, 10)
     def __init__(self, parent):
         tk.Frame.__init__(self, parent=None)
         self.parent = parent
+        parent.title("First")
         self.make_widgets()
     
     def make_widgets(self):
         for n in range(10):
             lbl = tk.Label(self, text="wat")
             lbl.grid(row=n, column=n, sticky='')
+
+# this version will give the exact same result as the above
+class SecondWindow(tk.Toplevel):
+    @grid(2000, 10)
+    def __init__(self, parent):
+        tk.Toplevel.__init__(self, parent=None)
+        self.parent = parent
+        self.title("Second")
+        self.make_widgets()
+    
+    def make_widgets(self):
+        for n in range(10):
+            lbl = tk.Label(self, text="wat")
+            lbl.grid(row=n, column=n, sticky='') 
+
+# this version will ALSO give the same result
+class ThirdWindow(tk.Toplevel):
+    @grid(2, 2)
+    def __init__(self, parent):
+        tk.Toplevel.__init__(self, parent=None)
+        self.parent = parent
+        self.title("Third")
+        self.make_widgets()
+    
+    def make_widgets(self):
+        for n in range(10):
+            lbl = tk.Label(self, text="wat")
+            lbl.grid(row=n, column=n, sticky='') 
+
 
 if __name__ == '__main__':
     app = TkRoot()
